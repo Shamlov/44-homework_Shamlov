886188b4a29d28c9270285facd49a829a880ea72
Shamlov <Pshamlov@gmail.com>
2024-06-19 22:12:15 +0300

    v2

--- js/script.js
+++ js/script.js
@@ -2,20 +2,82 @@
 Предоставить пользователю возможность изменять положение 
 синего указателя.*/
 
-let slider = document.getElementById('slider')
-let pointer = document.getElementById('pointer')
-function movePointerClick(e) {
-    if(e.target.closest('#pointer')) {
+const slider = document.getElementById('slider')
+const pointer = document.getElementById('pointer')
+const pointerRect = pointer.getBoundingClientRect()
+
+let dragEl = null
+
+pointer.addEventListener('mousedown',  () => {
+    dragEl = pointer
+})
+
+document.body.addEventListener('mouseup', () => {
+    dragEl = null
+})
+
+document.body.addEventListener('mousemove', (e) => {
+    if(!dragEl) {
         return
     }
-    pointer.style.cssText = `left: ${e.layerX - 5}px;` 
-    console.log(e.layerX)
+
+    dragEl.style.left = e.x - pointerRect.x - dragEl.clientWidth / 2 + 'px'
+    if(dragEl.style.left <= 0 + 'px') {
+        dragEl.style.left = '0px'
+    }
+})
+
+// function movePointerClick(e) {
+//     if(e.target.closest('#pointer')) {
+//         return
+//     }
+//     pointer.style.cssText = `left: ${e.layerX - 5}px;` 
+//     console.log(e.layerX)
+// }
+
+// function movePointerSlide (e) {
+//     pointer.style.cssText = `left: ${e.layerX - 5}px;`
+// }
+
+// slider.addEventListener('click', movePointerClick)
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+/*Создать html-страницу с галереей. 
+В один момент времени на экране отображается одно изо-
+бражение и две кнопки: Назад и Вперед. При нажатии на кнопки 
+изображения должны переключатся в указанном порядке. Когда 
+следующего/предыдущего изображения нет, то соответствующую 
+кнопку необходимо блокировать. Изображения хранить в заранее 
+подготовленном массиве. */
+
+const btnLeft = document.querySelector('#btnLeft')
+const btnRight = document.querySelector('#btnRight')
+const img = document.querySelector('#img')
+let imgArr = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+]
+
+
+let currentSlide = 1
+img.setAttribute('src', imgArr[currentSlide])   // фото по умолчанию
+
+
+function slideLeft () {
+    img.setAttribute('src', imgArr[currentSlide - 1])
+    currentSlide -= 1
 }
 
-function movePointerSlide (e) {
-    pointer.style.cssText = `left: ${e.layerX - 5}px;`
+function slideRight () {
+    img.setAttribute('src', imgArr[currentSlide + 1])
+    currentSlide += 1
 }
 
-slider.addEventListener('click', movePointerClick)
-slider.addEventListener('mousedown' , movePointerSlide)
+btnLeft.addEventListener('click', slideLeft)
+btnRight.addEventListener('click', slideRight)
+
 
